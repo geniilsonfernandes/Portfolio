55d772b90cdd22f047e247f78d473ac1f2936bde
genilson <geniilsonfernandes@gmail.com>
2023-04-21 02:01:13 -0300

    feat: create menu component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,69 +1,13 @@
-import MediaComponent from 'components/MediaComponent'
+import Menu from 'components/Menu'
 import * as S from './styles'
-import { RiArrowDropDownLine } from 'react-icons/ri'
-import useModal from 'hook/useModal'
 
 const Header = () => {
-  const { closeModal, openModal, isOpen } = useModal()
-
-  const handleOpenMenu = () => {
-    openModal()
-  }
-
-  const handleCloseMenu = () => {
-    closeModal()
-  }
-
-  const handleMenuClick = () => {
-    if (isOpen) {
-      handleCloseMenu()
-    } else {
-      handleOpenMenu()
-    }
-  }
-
   return (
     <S.Wrapper>
       <S.LogoWrapper>
         <S.LogoIcon />
       </S.LogoWrapper>
-      <S.MenuWrapper>
-        <MediaComponent mediaQuery="(max-width: 768px)">
-          <S.Button onClick={handleOpenMenu}>
-            Menu <RiArrowDropDownLine size={20} />
-          </S.Button>
-        </MediaComponent>
-
-        <MediaComponent mediaQuery="(min-width: 768px)">
-          <S.MenuLink href="#home">Home</S.MenuLink>
-          <S.MenuLink href="#projects">Projects</S.MenuLink>
-          <S.MenuLink href="#about">About</S.MenuLink>
-          <S.MenuLink href="#skills">Skills</S.MenuLink>
-        </MediaComponent>
-      </S.MenuWrapper>
-      {isOpen && (
-        <S.Overlay>
-          <S.OverlayClick onClick={handleCloseMenu} />
-          <S.MenuFullWrapper>
-            <S.MenuFullHead>
-              <S.CloseMenu onClick={handleCloseMenu} />
-            </S.MenuFullHead>
-
-            <S.MenuLink href="#home" onClick={handleMenuClick}>
-              Home
-            </S.MenuLink>
-            <S.MenuLink href="#projects" onClick={handleMenuClick}>
-              Projects
-            </S.MenuLink>
-            <S.MenuLink href="#about" onClick={handleMenuClick}>
-              About
-            </S.MenuLink>
-            <S.MenuLink href="#skills" onClick={handleMenuClick}>
-              Skills
-            </S.MenuLink>
-          </S.MenuFullWrapper>
-        </S.Overlay>
-      )}
+      <Menu />
     </S.Wrapper>
   )
 }
