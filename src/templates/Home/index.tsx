eb957d8897173e335fe5c90d71e59adff743b226
Genilson fernandes <77983928+geniilsonfernandes@users.noreply.github.com>
2024-01-29 08:21:45 -0300

    Update index.tsx

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -30,15 +30,15 @@ const portfolio = [
     photo: './img/money_app.png',
     repo: 'https://github.com/geniilsonfernandes/moneyguard',
   },
-  {
-    description:
-      'O Habbit App é um gerenciador de hábitos desenvolvido em Next.js. Esta aplicação foi criada para permitir aos usuários organizarem e acompanharem seus hábitos diários de forma eficiente, ajudando na construção de rotinas mais produtivas e saudáveis.',
-    id: '2324543',
-    link: 'https://github.com/geniilsonfernandes/habbit-app',
-    name: 'Habbit App',
-    photo: './img/habit_app.png',
-    repo: 'https://github.com/geniilsonfernandes/habbit-app',
-  },
+  // {
+  //   description:
+  //     'O Habbit App é um gerenciador de hábitos desenvolvido em Next.js. Esta aplicação foi criada para permitir aos usuários organizarem e acompanharem seus hábitos diários de forma eficiente, ajudando na construção de rotinas mais produtivas e saudáveis.',
+  //   id: '2324543',
+  //   link: 'https://github.com/geniilsonfernandes/habbit-app',
+  //   name: 'Habbit App',
+  //   photo: './img/habit_app.png',
+  //   repo: 'https://github.com/geniilsonfernandes/habbit-app',
+  // },
   {
     description:
       ' Dashschool é um gerenciador de cursos construído com Next.js e Prisma, utilizando o framework Chakra UI para estilização. Este projeto tem como objetivo fornecer uma plataforma para organizar e acessar cursos de maneira intuitiva e eficaz.',
@@ -47,25 +47,25 @@ const portfolio = [
     name: 'Dashschool',
     photo: './img/dashschool_app.png',
     repo: 'https://github.com/geniilsonfernandes/dashschool',
-  },
-  {
-    description:
-      'Uma aplicação desenvolvida para praticar conceitos avançados do React, aplicar Test-Driven Development (TDD) e melhorar a qualidade do código. Além disso, explora o uso de ferramentas como Storybook para visualização otimizada dos componentes.',
-    id: '5435rf',
-    link: 'https://weather-app-react-s.vercel.app/',
-    name: 'Weather App react',
-    photo: './img/weather_app.png',
-    repo: 'https://github.com/geniilsonfernandes/weather-app-react-s',
-  },
-  {
-    description:
-      'Colorise Palette é um repositório de paletas de cores, permitindo aos usuários salvar e remover suas paletas favoritas. Esta aplicação oferece uma maneira simples e intuitiva de explorar e gerenciar diversas combinações de cores.',
-    id: '5435rfswreewr',
-    link: 'https://colorise-palette.vercel.app/',
-    name: 'Colorise Palette',
-    photo: './img/colorise_banne.png',
-    repo: 'https://github.com/geniilsonfernandes/colorise-palette',
-  },
+  }
+  // {
+  //   description:
+  //     'Uma aplicação desenvolvida para praticar conceitos avançados do React, aplicar Test-Driven Development (TDD) e melhorar a qualidade do código. Além disso, explora o uso de ferramentas como Storybook para visualização otimizada dos componentes.',
+  //   id: '5435rf',
+  //   link: 'https://weather-app-react-s.vercel.app/',
+  //   name: 'Weather App react',
+  //   photo: './img/weather_app.png',
+  //   repo: 'https://github.com/geniilsonfernandes/weather-app-react-s',
+  // },
+  // {
+  //   description:
+  //     'Colorise Palette é um repositório de paletas de cores, permitindo aos usuários salvar e remover suas paletas favoritas. Esta aplicação oferece uma maneira simples e intuitiva de explorar e gerenciar diversas combinações de cores.',
+  //   id: '5435rfswreewr',
+  //   link: 'https://colorise-palette.vercel.app/',
+  //   name: 'Colorise Palette',
+  //   photo: './img/colorise_banne.png',
+  //   repo: 'https://github.com/geniilsonfernandes/colorise-palette',
+  // },
 ] as Omit<ProjectProps, 'size' | 'isHighlighted'>[]
 
 const Home = ({ user }: HomeProps) => {
